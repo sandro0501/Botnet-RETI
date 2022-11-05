6142d0485e312a00b096afc74518d4fa1949acfd
SantoloBarretta <san.barretta@studenti.unina.it>
2022-11-05 17:29:47 +0100

    Aggiunto metodo getNetworkInfo. Ulteriori modifiche minimali.

--- Server1.py
+++ Server1.py
@@ -50,7 +50,9 @@ def menu():
           +"3: ricevi informazioni sul tempo d'avvio\n"
           +"4: ricevi informazioni sulla memoria RAM\n"
           +"5: ricevi informazioni sul disco\n"
-          +"6: entra in controllo della bash\n")
+          +"6: ricevi informazioni sulla scheda di rete e interfacce di rete\n"
+          +"7: entra in controllo della bash\n"
+          +"8: riepilogo informazioni\n")
     return input()
     
 while True:
@@ -61,12 +63,12 @@ while True:
     while True:
         comando = menu()
 
-        if comando == '6':
+        if comando == '7':
             cmd = ''
             while cmd.lower() != "exit": #Finche non scegliamo di uscire dalla bash
                 cmd = input(f"{cwd}$>") #stampiamo la cwd, prendiamo il comando da eseguire e lo mandiamo
                 if cmd.lower() != "exit":
-                    if (not(invia_messaggio(f"6<sep>{cmd}"))): break
+                    if (not(invia_messaggio(f"7<sep>{cmd}"))): break
                     output = ricevi_messaggio()
                     queryRes, cwd = output.split('<sep>')
                     print(queryRes + "\n")
